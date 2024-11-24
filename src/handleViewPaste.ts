eb78e01a73c5f97585a5f2b64aa556322ed2ada5
Brendan Viscomi <brendanv@users.noreply.github.com>
2024-11-24 03:31:32 +0000

    Add syntax highlighting

--- src/handleViewPaste.ts
+++ src/handleViewPaste.ts
@@ -1,52 +1,79 @@
 import pasteViewPage from './pages/pasteView';
 import type { AuthResult } from './requireAuth';
 
-export default async function handleViewPaste(
-  request: Request,
-  env: Env,
-  url: URL,
-  authCheck: AuthResult
-): Promise<Response> {
-  // We already checked that it starts with /p/
-  const pathParts = url.pathname.substring(3).split('/');
-  const pasteId = pathParts[0];
-  const format = pathParts[1];
+export default async function handleViewPaste(request: Request, env: Env, url: URL, authCheck: AuthResult): Promise<Response> {
+	// We already checked that it starts with /p/
+	const pathParts = url.pathname.substring(3).split('/');
+	const pasteId = pathParts[0];
+	const format = pathParts[1];
 
-  const { value: paste, metadata } = await env.PASTE_KV.getWithMetadata(pasteId);
-  
-  if (!paste) {
-    return new Response('Not Found.', { status: 404 });
-  }
+	const { value: paste, metadata } = await env.PASTE_KV.getWithMetadata(pasteId);
 
-  // Check auth for private pastes
-  if (metadata?.visibility !== 'public') {
-    if (!authCheck.isAuthorized) {
-      return authCheck.response;
-    }
-  }
+	if (!paste) {
+		return new Response('Not Found.', { status: 404 });
+	}
 
-  // Handle different formats
-  switch (format) {
-    case 'raw':
-      return new Response(paste, {
-        headers: { 'content-type': 'text/plain;charset=UTF-8' }
-      });
+	// Check auth for private pastes
+	if (metadata?.visibility !== 'public') {
+		if (!authCheck.isAuthorized) {
+			return authCheck.response;
+		}
+	}
 
-    default:
-      // Default HTML view
+	const domain = env.DOMAIN;
+	const githubRepoUrl = env.GH_REPO_URL;
+	const title = metadata?.title;
+	const createdAt = metadata?.createdAt;
+	const createdBy = metadata?.createdBy;
+	const expirationSeconds = metadata?.expirationSeconds;
+	const visibility = metadata?.visibility === 'public' ? 'public' : 'authorized';
+
+	switch (format) {
+		case 'raw':
+			return new Response(paste, {
+				headers: { 'content-type': 'text/plain;charset=UTF-8' },
+			});
+
+		case 'js':
+		case 'html':
+		case 'python':
+		case 'ruby':
+		case 'php':
+		case 'css':
+		case 'sql':
       return new Response(
-        pasteViewPage({
-          domain: env.DOMAIN,
-          githubRepoUrl: env.GH_REPO_URL,
-          title: metadata?.title,
-          createdAt: metadata?.createdAt,
-          createdBy: metadata?.createdBy,
-          expirationSeconds: metadata?.expirationSeconds,
-          visibility: metadata?.visibility === 'public' ? 'public' : 'authorized',
-          paste,
-        }), {
-          headers: { 'content-type': 'text/html;charset=UTF-8' }
-        }
-      );
-  }
+				pasteViewPage({
+					domain,
+					githubRepoUrl,
+					title,
+					createdAt,
+					createdBy,
+					expirationSeconds,
+					visibility,
+					highlightLang: format,
+					paste,
+				}),
+				{
+					headers: { 'content-type': 'text/html;charset=UTF-8' },
+				}
+			);
+
+		default:
+			return new Response(
+				pasteViewPage({
+					domain,
+					githubRepoUrl,
+					title,
+					createdAt,
+					createdBy,
+					expirationSeconds,
+					visibility,
+					highlightLang: null,
+					paste,
+				}),
+				{
+					headers: { 'content-type': 'text/html;charset=UTF-8' },
+				}
+			);
+	}
 }
--- src/pages/pasteView.ts
+++ src/pages/pasteView.ts
@@ -9,11 +9,22 @@ type Args = {
 	createdAt: string | null;
 	createdBy: string | null;
 	expirationSeconds: number | null;
-	visibility: Visibility,
+	visibility: Visibility;
+	highlightLang: string | null;
 	paste: string;
 };
 
-const newPastePage = ({ domain, githubRepoUrl, title, createdAt, createdBy, expirationSeconds, visibility, paste }: Args) => {
+const newPastePage = ({
+	domain,
+	githubRepoUrl,
+	title,
+	createdAt,
+	createdBy,
+	expirationSeconds,
+	visibility,
+	highlightLang,
+	paste,
+}: Args) => {
 	const parsedCreatedAt = createdAt ? new Date(createdAt) : null;
 	const parsedExpiresAt = parsedCreatedAt && expirationSeconds ? new Date(parsedCreatedAt.getTime() + expirationSeconds * 1000) : null;
 
@@ -28,12 +39,24 @@ const newPastePage = ({ domain, githubRepoUrl, title, createdAt, createdBy, expi
 		metadataItems.push(`Expires: ${parsedExpiresAt.toLocaleString()}`);
 	}
 
+	let extraIncludes = '';
+	let prismClassname = '';
+	if (highlightLang) {
+		extraIncludes = [
+			`<script src="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/prism.min.js"></script>`,
+			`<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prism-themes@1.9.0/themes/prism-dracula.min.css">`,
+		].join('\n');
+		prismClassname = `language-${he.escape(highlightLang)}`;
+	}
+
 	return content
 		.replaceAll('{{DOMAIN}}', he.escape(domain))
 		.replaceAll('{{GH_REPO_URL}}', he.escape(githubRepoUrl))
 		.replaceAll('{{TITLE}}', he.escape(title || 'Untitled Paste'))
 		.replaceAll('{{METADATA_ITEMS}}', he.escape(metadataItems.join(' • ')))
 		.replaceAll('{{PRIVACY}}', visibility === 'public' ? 'Public' : 'Logged-in users only')
+		.replaceAll('{{EXTRA_HEAD_CONTENT}}', extraIncludes)
+		.replaceAll('{{PRISM_CLASSNAME}}', prismClassname)
 		.replaceAll('{{PASTE_CONTENT}}', he.escape(paste));
 };
 
